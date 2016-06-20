5402082955ebea328be7f8289718eb5faacaaac8
Ben Lambert <ben@benjaminlambert.com>
2016-06-20 02:49:47 -0400

    Preparing to submit to PyPI

--- setup.py
+++ setup.py
@@ -3,11 +3,14 @@ from distutils.core import setup
 setup(
     name='EditDistance',
     version='0.1.0',
-    author='Benjamin Lambert',
+    author='Ben Lambert',
     author_email='ben@benjaminlambert.com',
     packages=['editdistance'],
     license='LICENSE.txt',
     description='Computing edit distance on arbitrary Python sequences.',
     long_description=open('README.md').read(),
-    install_requires=[],
+    url='https://github.com/belambert/editdistance',
+    download_url = 'https://github.com/belambert/editdistance/tarball/0.1', 
+    keywords=['edit', 'distance', 'levenshtein'],
+    classifiers = [],
 )
