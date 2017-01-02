fa1c729cd9af91bd8ded7b207d8b989942c4616e
Ben Lambert <ben@benjaminlambert.com>
2017-01-02 03:05:20 -0500

    Rebuilt Sphinx config without extensions.

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # edit-distance documentation build configuration file, created by
-# sphinx-quickstart on Mon Jan  2 03:02:03 2017.
+# sphinx-quickstart on Mon Jan  2 03:04:49 2017.
 #
 # This file is execfile()d with the current directory set to its
 # containing dir.
@@ -28,16 +28,7 @@ import os
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.doctest',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.todo',
-    'sphinx.ext.coverage',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages',
-]
+extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -110,7 +101,7 @@ pygments_style = 'sphinx'
 #keep_warnings = False
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
-todo_include_todos = True
+todo_include_todos = False
 
 
 # -- Options for HTML output ----------------------------------------------
@@ -293,7 +284,3 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
-
-
-# Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
--- docs/index.rst
+++ docs/index.rst
@@ -1,5 +1,5 @@
 .. edit-distance documentation master file, created by
-   sphinx-quickstart on Mon Jan  2 03:02:03 2017.
+   sphinx-quickstart on Mon Jan  2 03:04:49 2017.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
