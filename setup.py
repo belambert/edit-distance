d79f9bd0d70f92416a8c26c4f101f47c99717feb
Ben Lambert <ben@benjaminlambert.com>
2018-10-22 00:31:41 -0400

    Upload the README to PyPI, and updated email address.

--- setup.py
+++ setup.py
@@ -2,15 +2,17 @@ from setuptools import setup
 
 setup(
     name='edit_distance',
-    version='1.0.2.dev0',
+    version='1.0.3',
     author='Ben Lambert',
-    author_email='ben@benjaminlambert.com',
+    author_email='blambert@gmail.com',
     packages=['edit_distance'],
     license='LICENSE.txt',
     description='Computing edit distance on arbitrary Python sequences.',
     url='https://github.com/belambert/editdistance',
     keywords=['edit', 'distance', 'editdistance', 'levenshtein'],
     test_suite='test.test.TestEditDistance',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Environment :: Console",
