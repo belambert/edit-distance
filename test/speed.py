353b6a0980efe9077df029f31026e0c6fb59ba49
Ben Lambert <blambert@gmail.com>
2019-12-04 16:28:10 -0500

    Add a test of two very long random sequences. (#11)

--- test/speed.py
+++ test/speed.py
@@ -1,4 +1,4 @@
-# Copyright 2013-2018 Ben Lambert
+# Copyright 2013-2019 Ben Lambert
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -26,12 +26,14 @@ sequences = [(['a', 'b'] * 13, ['a', 'c', 'd', 'a', 'b'] * 5),
 # Functions to test
 run_strings = ['edit_distance.edit_distance(a, b)',
                'edit_distance.edit_distance_backpointer(a, b)']
-               
-for a, b in sequences:
-    print('For sequences: a={}, b={}'.format(a, b))
-    setup_string = "import edit_distance; a = {}; b = {}".format(a, b)
-    for run_string in run_strings:
-        print('For: {}'.format(run_string))
-        t = timeit.Timer(run_string, setup=setup_string)
-        total_time = t.timeit(number=n)
-        print('Average time: {:.4} ms'.format(total_time/n * 1000))
+
+def speed_tests():
+
+    for a, b in sequences:
+        print('For sequences: a={}, b={}'.format(a, b))
+        setup_string = "import edit_distance; a = {}; b = {}".format(a, b)
+        for run_string in run_strings:
+            print('For: {}'.format(run_string))
+            t = timeit.Timer(run_string, setup=setup_string)
+            total_time = t.timeit(number=n)
+            print('Average time: {:.4} ms'.format(total_time/n * 1000))
