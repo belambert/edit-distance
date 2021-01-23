ec8a71b6ecb4546ac6e2acd8fdc8f6d8500765c8
Ben Lambert <blambert@gmail.com>
2021-01-23 13:54:39 -0500

    black formatting for test files and setup.py

--- test/speed.py
+++ test/speed.py
@@ -18,22 +18,25 @@ import timeit
 n = 1000
 
 # Sequence pairs to benchmark with
-sequences = [(['a', 'b'] * 13, ['a', 'c', 'd', 'a', 'b'] * 5),
-             ("'fsffvfdsbbdfvvdavavavavavava'",
-              "'fvdaabavvvvvadvdvavavadfsfsdafvvav'"),
-             ([1, 2] * 13, [3, 2, 1, 2, 5] * 5)]
+sequences = [
+    (["a", "b"] * 13, ["a", "c", "d", "a", "b"] * 5),
+    ("'fsffvfdsbbdfvvdavavavavavava'", "'fvdaabavvvvvadvdvavavadfsfsdafvvav'"),
+    ([1, 2] * 13, [3, 2, 1, 2, 5] * 5),
+]
 
 # Functions to test
-run_strings = ['edit_distance.edit_distance(a, b)',
-               'edit_distance.edit_distance_backpointer(a, b)']
+run_strings = [
+    "edit_distance.edit_distance(a, b)",
+    "edit_distance.edit_distance_backpointer(a, b)",
+]
 
 
 def speed_tests():
     for a, b in sequences:
-        print('For sequences: a={}, b={}'.format(a, b))
+        print("For sequences: a={}, b={}".format(a, b))
         setup_string = "import edit_distance; a = {}; b = {}".format(a, b)
         for run_string in run_strings:
-            print('For: {}'.format(run_string))
+            print("For: {}".format(run_string))
             t = timeit.Timer(run_string, setup=setup_string)
             total_time = t.timeit(number=n)
-            print('Average time: {:.4} ms'.format(total_time/n * 1000))
+            print("Average time: {:.4} ms".format(total_time / n * 1000))
