553108ca90b380516ea2b4b8e95b23baea80c414
Ben Lambert <blambert@gmail.com>
2025-09-27 23:41:40 -0400

    add pyupgrade check

--- edit_distance/edit_distance.py
+++ edit_distance/edit_distance.py
@@ -20,7 +20,8 @@ Code for computing edit distances.
 
 import operator
 import sys
-from typing import Optional, Sequence
+from typing import Optional
+from collections.abc import Sequence
 
 INSERT: str = "insert"
 DELETE: str = "delete"
